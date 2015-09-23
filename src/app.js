e1577ceeab7254052e66296bba6e075c532fcfb3
A Boyd <avboyd@gmail.com>
2015-09-22 19:01:38 -0500

    WIP commit, ajax troubles
    Trying to figure out what the hell I can do about getting an ajax call to return data to me outside of it.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 /**
- * Welcome to Pebble.js!
+ * Hurk
  *
- * This is where you write your app.
+ * hurk!
  */
 
 var ajax =  require('ajax');
@@ -10,23 +10,33 @@ var UI = require('ui');
 var Vector2 = require('vector2');
 var Clock = require('clock');
 
-var 
+var regularRotation = false;
+var RR = function() {
+    return regularRotation;
+};
 
-//var mainWindow = new UI.Window({
-//    fullscreen: true,
-//});
+var Holding = function () {
+    this.value = null;
+};
 
-var getMaps = function() {			
-	return ajax(
+var holder = new Holding();
+
+var getMaps = function(holder) {
+
+    var iResponse = holder;
+    
+    ajax(
         {
             url: 'http://splatoon.ink/schedule.json',
-            type: 'GET',
+            type: 'json',
+            method: 'GET',
         },
         function(data, status, request) {
             console.log("response successful");
-            console.log("data: " +  data);
-            console.log("status: " + status);
-            console.log("request: " + request);
+            console.log(data.schedule[0].toString());
+            console.log("data: " + data);
+            iResponse.schedule = data.schedule;
+            console.log(iResponse.schedule[0].ranked.rulesEN);
         },
         function(data, status, request) {
             console.log("response failure");
@@ -34,22 +44,29 @@ var getMaps = function() {
             console.log("status: " + status);
             console.log("request: " + request);
         }
-	);
+    );
+    
+    console.log(iResponse.schedule[0].ranked.rulesEN);
+    
+    var inkNum = 0;
+    for (inkNum = 0; inkNum < iResponse.schedule.length; inkNum++) {
+        if (iResponse.schedule[inkNum].regular.hasOwnProperty("rulesEN")) {
+        }
+        else {
+            iResponse.schedule[inkNum].regular.rulesEN = "Turf War";
+        }
+    }
+    
+    return iResponse;
 };
 
-var inkResponse = getMaps();
+var inkResponse = getMaps(holder);
 
-var regular_rotation = false
-var RR = function() {return regular_rotation};
 
 // This is a bit of future-proofing. If Ninty adds alternate modes to Random
 // matches in the future, they will presumably be formatted the same way as
 // the Ranked rules data is now. This will catch the alternate rules data if
 // it exists.
-
-if (inkResponse.schedule[0].maps.hasOwnProperty("rulesEN")) {
-    regular_rotation = true;
-}
 
 var scheduledMaps = function() {
     this.startTime = null;
@@ -71,6 +88,7 @@ scheduledMaps.prototype.update = function() {};
 // prevMaps.consume(currMaps.feed());
 // currMaps.consume(nextMaps.feed());
 // nextMaps.consume(lastMaps.feed());
+
 scheduledMaps.prototype.feed = function() {
     return {
         startTime: this.startTime,
@@ -89,23 +107,134 @@ scheduledMaps.prototype.start = function() {};
 scheduledMaps.prototype.end = function() {};
 
 
-var inkData = {
-    hasMaps: function() {},
+var inkData = function() {
+    this.state = {
+        waiting: true,
+        waitingFor: "startup",
+    };
+        
+    this.mapState = {
+        prevMaps: "new",
+        currMaps: "new",
+        nextMaps: "new",
+        lastMaps: "new",
+    };
     
-    hasPrev: false,
-    hasCurr: false,
-    hasNext: false,
-    hasLast: false,
+    this.prevMaps = new scheduledMaps();
+    this.currMaps = new scheduledMaps();
+    this.nextMaps = new scheduledMaps();
+    this.lastMaps = new scheduledMaps();
     
-    
-    prevMaps: new scheduledMaps(),
-    currMaps: new scheduledMaps(),
-    nextMaps: new scheduledMaps(),
-    lastMaps: new scheduledMaps(),
 };
 
-
 console.log(inkResponse);
+
+inkData.prototype.processInkResponse = function(iResp) {
+    var prepData = function(data) {
+        var newData = [];
+        var stdData =  true;
+        var mapBase = {
+            startTime: null,
+            endTime: null,
+            regular: {
+                maps: [],
+                rules: "",
+            },
+            ranked: {
+                maps: [],
+                rules: "",
+            },
+        };
+        
+        if (data.schedule.length == 3) {
+            console.log("Prepping standard rotation data.");
+        }
+        else {
+            console.log("Prepping nonstandard rotation data. " +
+                        "Schedule length is " + data.schedule.length);
+            stdData = false;
+        }
+        
+        for (var iMap = 0; iMap < data.schedule.length; iMap++) {
+            mapBase.startTime = data.schedule[iMap].startTime;
+            mapBase.endTime = data.schedule[iMap].endTime;
+            mapBase.regular.maps = data.schedule[iMap].regular.maps;
+            mapBase.regular.rules = data.schedule[iMap].regular.rulesEN;
+            mapBase.ranked.maps = data.schedule[iMap].ranked.maps;
+            mapBase.ranked.rules = data.schedule[iMap].ranked.rulesEN;
+            newData.push(mapBase);
+        }
+        
+        return newData;
+    };
+    
+    var newData = prepData(iResp);
+    for (var myMap in this.mapState) {
+        switch (myMap) {
+            case "prevMaps":
+                if (this.mapState[myMap] == "new") {
+                    this.mapState[myMap] = "empty";
+                    console.log("prevMaps marked as empty");
+                }
+                else if (this.mapState[myMap] == "rotate") {
+                    this[myMap].consume(this.currMaps.feed());
+                    console.log("Rotating currMaps into prevMaps.");
+                    this.mapState[myMap] = "waiting";
+                }
+                break;
+            case "currMaps":
+                if (this.mapState[myMap] == "new") {
+                    this[myMap].consume(newData[0]);
+                    console.log("Adding new currMaps to rotation.");
+                    this.mapState[myMap] = "waiting";
+                }
+                else if (this.mapState[myMap] == "rotate") {
+                    this[myMap].consume(this.nextMaps.feed());
+                    console.log("Rotating nextMaps into currMaps.");
+                    this.mapState[myMap] = "waiting";
+                }
+                break;
+            case "nextMaps":
+                if (this.mapState[myMap] == "new") {
+                    this[myMap].consume(newData[1]);
+                    console.log("Adding new nextMaps to rotation.");
+                    this.mapState[myMap] = "waiting";
+                }
+                else if (this.mapState[myMap] == "rotate") {
+                    this[myMap].consume(this.lastMaps.feed());
+                    console.log("Rotating lastMaps into nextMaps.");
+                    this.mapState[myMap] = "waiting";
+                }
+                break;
+            case "lastMaps":
+                if (this.mapState[myMap] == "new") {
+                    this[myMap].consume(newData[2]);
+                    console.log("Adding new lastMaps to rotation.");
+                    this.mapState[myMap] = "waiting";
+                }
+                else if (this.mapState[myMap] == "rotate") {
+                    this[myMap].consume(newData[2]);
+                    console.log("Adding new lastMaps to rotation.");
+                    this.mapState[myMap] = "waiting";
+                }
+                break;
+        }
+    }
+};
+
+var splatime = new inkData();
+inkData.processInkResponse(inkResponse);
+
+//var monitor = new UI.Window({
+//    fullscreen: true,
+//});
+var monitor = new UI.Card({
+    title: 'Splatime!',
+    subtitle: 'TW maps:',
+    body: splatime.currMaps.maps[0] + ", " + splatime.currMaps[1],
+});
+
+monitor.show();
 
 //var now = new Date();
 //var timezone = now.getTimezoneOffset();
